40173ef1ccb9fb3fe970e8150dbd4d4046358077
McConnell <ik.ugwuanyi@gmail.com>
2023-05-27 19:37:56 +0100

    added models and base url patterns

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    description = models.TextField(max_length=1000, default="")
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reservation_date = models.DateTimeField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self):
+        return self.user.first_name
